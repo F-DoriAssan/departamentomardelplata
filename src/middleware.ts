42b6e7768a52e8250267b7a656aa2925175c1220
Federico Dorigatti Assan <114174311+F-DoriAssan@users.noreply.github.com>
2024-06-18 11:59:13 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,5 +10,5 @@ export default createMiddleware({
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['departamentomardelplata.com/', '/(|en|es|pt|)/:path*']
+  matcher: ['/', '/(|en|es|pt|)/:path*']
 };
